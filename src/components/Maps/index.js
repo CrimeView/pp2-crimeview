aa14934e6eb27cceb27065129bbf2299b94bc52a
Adyson Alves <adyson6@gmail.com>
2023-04-25 13:04:48 -0300

    Mapa com geolocalização

--- src/components/Maps/index.js
+++ src/components/Maps/index.js
@@ -1,31 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import GoogleMapReact from 'google-map-react';
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 export default function SimpleMap(){
+  const [latitude, setLatitude] = useState();
+  const [longitude, setLongitude] = useState();
+
+  function handleLocation(position) {
+    const latitudeUser = position.coords.latitude;
+    const longitudeUser = position.coords.longitude;
+    setLatitude(latitudeUser);
+    setLongitude(longitudeUser);
+  }
+
+  function handleError(error) {
+    console.error(error);
+  }
+
+  function getLocation() {
+    navigator.geolocation.getCurrentPosition(handleLocation, handleError);
+  }
+
+  if(latitude === undefined || longitude === undefined){
+    getLocation();
+    return
+  }
+
+
   const defaultProps = {
     center: {
-      lat: -8.1146232,
-      lng: -35.0369299
+      lat: latitude,
+      lng: longitude
     },
     zoom: 16
   };
 
   return (
     // Important! Always set the container height explicitly
-    <div style={{ height: '100vh', width: '100%' }}>
+    <div style={{ height: '100vh', width: '100%' }}>   
       <GoogleMapReact
         bootstrapURLKeys={{ key: "AIzaSyDcFwwj4jzI2nZSOH1CGtgwRSDsdlM_HXU" }}
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
       >
         <AnyReactComponent
-          lat={-8.1146232}
-          lng={-35.0369299}
+          lat={latitude}
+          lng={longitude}
           text="Sua localização"
         />
+
       </GoogleMapReact>
+
     </div>
   );
 }
