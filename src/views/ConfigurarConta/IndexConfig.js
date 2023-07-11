3dede46b21c864777768fc6dbb337aff8ee3964a
adysonalves <adyson6@gmail.com>
2023-07-11 17:33:26 -0300

    Correções

--- src/views/ConfigurarConta/IndexConfig.js
+++ src/views/ConfigurarConta/IndexConfig.js
@@ -45,43 +45,20 @@ function ConfigurarConta() {
 
                 
                 <Form className="register-form">
-                  <label>Nome</label>
-                  <InputGroup className="form-group-no-border">
-                    <InputGroupAddon addonType="prepend">
-                      {/* <InputGroupText>
-                        <i className="nc-icon nc-email-85" />
-                      </InputGroupText>*/}
-                    </InputGroupAddon>
-                    <Input placeholder="Nome" type="text" />
-                  </InputGroup>
+                  
 
-
-                  <label>Sobrenome</label>
+                  <label>E-mail</label>
                   <InputGroup className="form-group-no-border">
                     <InputGroupAddon addonType="prepend">
                     </InputGroupAddon>
-                    <Input placeholder="Sobrenome" type="text" />
+                    <Input placeholder="E-mail" type="password" />
                   </InputGroup>
 
-                  <label>Senha Atual</label>
-                  <InputGroup className="form-group-no-border">
-                    <InputGroupAddon addonType="prepend">
-                    </InputGroupAddon>
-                    <Input placeholder="Senha atual" type="password" />
-                  </InputGroup>
-
-                  <label>Nova Senha</label>
+                  <label>Senha</label>
                   <InputGroup className="form-group-no-border">
                     <InputGroupAddon addonType="prepend">
                     </InputGroupAddon>
                     <Input placeholder="Nova senha" type="password" />
-                  </InputGroup>
-
-                  <label>Repetir Senha</label>
-                  <InputGroup className="form-group-no-border">
-                    <InputGroupAddon addonType="prepend">
-                    </InputGroupAddon>
-                    <Input placeholder="Repetir senha" type="password" />
                   </InputGroup>
 
                   <Button
